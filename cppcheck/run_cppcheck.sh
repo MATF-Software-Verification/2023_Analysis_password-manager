bab9e211300829948a1192e2d628d937327ea7cc
emilija <emilijstevanovic@github.com>
2024-08-20 18:05:52 +0200

    cppcheck dodat

---
+++ cppcheck/run_cppcheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SOURCE_DIR="../password-manager-master/source"
+
+
+echo "checking console..."
+cppcheck --enable=all --inconclusive --suppress=missingIncludeSystem --output-file=console.txt  $SOURCE_DIR/console
+
+echo "checking ui..."
+cppcheck --enable=all --inconclusive --suppress=missingIncludeSystem --output-file=ui.txt  $SOURCE_DIR/ui
+
+
